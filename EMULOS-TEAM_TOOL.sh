d39055ce3e97fa6ea935e689add2084217ea45c8
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2019-12-27 15:29:10 +0000

    Update EMULOS-TEAM_TOOL.sh
    RETROARCH UPGRADE VERSION TO 1.8.2

--- EMULOS-TEAM_TOOL.sh
+++ EMULOS-TEAM_TOOL.sh
@@ -140,7 +140,7 @@ exit
 # }
 
 function RPI4_retroarch_instalador() {                                          
-dialog --infobox "... RIP4 BETA Script instalador de Retroarch en su version 1.8.1 ..." 30 55 ; sleep 5
+dialog --infobox "... RIP4 BETA Script instalador de Retroarch en su version 1.8.2 ..." 30 55 ; sleep 5
 sudo apt-get update
 dialog --infobox "... Iniciando actualizacion del sistema y sus paquetes ,espere... ..." 30 55 ; sleep 5
 sudo apt-get upgrade -y
@@ -156,14 +156,14 @@ dialog --infobox "... Compilar e instalar RetroArch ,iniciando espere! ..." 30 5
 sudo apt-get install -y build-essential libasound2-dev libudev-dev libxkbcommon-dev zlib1g-dev libfreetype6-dev libegl1-mesa-dev libgles2-mesa-dev libgbm-dev libavcodec-dev libsdl2-dev libsdl-image1.2-dev libxml2-dev yasm libavformat-dev libavdevice-dev libswresample-dev libavresample-dev libswscale-dev libv4l-dev libgl*-mesa-dev
 #build-essential libxkbcommon-dev zlib1g-dev libfreetype6-dev libegl1-mesa-dev libasound2-dev libgles2-mesa-dev libgbm-dev libavcodec-dev libsdl2-dev libsdl-image1.2-dev libxml2-dev yasm libavformat-dev libavdevice-dev libswresample-dev libavresample-dev libswscale-dev libv4l-dev libgl*-mesa-dev
 #
-cd && curl -LO 'https://github.com/libretro/RetroArch/archive/v1.8.1.tar.gz' && tar -zxvf v1.8.1.tar.gz
-sudo rm v1.8.1.tar.gz
-cd RetroArch-1.8.1
+cd && curl -LO 'https://github.com/libretro/RetroArch/archive/v1.8.2.tar.gz' && tar -zxvf v1.8.2.tar.gz
+sudo rm v1.8.2.tar.gz
+cd RetroArch-1.8.2
 CFLAGS='-mfpu=neon -mtune=cortex-a72 -march=armv8-a' ./configure --disable-opengl1 --enable-neon --enable-opengles3 --enable-opengles --enable-udev --disable-videocore
 #CFLAGS="-mfpu=neon" ./configure --disable-videocore --enable-opengl --disable-opengl1 --enable-alsa --enable-udev --disable-opengles --enable-neon
 make -j4
 sudo make install
-cd && sudo rm -R RetroArch-1.8.1/
+cd && sudo rm -R RetroArch-1.8.2/
 
 ##### instalar bios base
 dialog --infobox "... Descargando y Copiando BIOS BASE para retroarch en /home/pi/.config/retroarch/system ..." 30 55 ; sleep 3
